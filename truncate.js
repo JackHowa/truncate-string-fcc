e8294958e15f2a3a7132895acf9f872eaa1d520c
jackhowa <johncarverhoward@gmail.com>
2017-08-09 15:33:54 -0500

    add checks for short strings, don't redefine variables

--- truncate.js
+++ truncate.js
@@ -1,7 +1,19 @@
 function truncateString(str, num) {
+  // real string length for limits less than 3 
   var realStringLength = num - 3;
-  var newString = str.slice(0,realStringLength) + "...";
-  return newString;
+
+  // check whether the num limit is less than string length 
+  // also that the num limit is greater than 3 so ellipses don't ruin
+  if (num < str.length && num > 3) {
+    str = str.slice(0, realStringLength) + "...";
+  }
+  // for small limits that would have the ellipses ruin 
+  else if (num < 3) {
+    str = str.slice(0, num) + "...";
+  }
+  
+  // return string 
+  return str;
 }
 
-truncateString("A-tisket a-tasket A green and yellow basket", 11);
+truncateString("A-", 1);
